837f7105cc6978338286f76499602dd52ed50138
codedbyelif <elifkyr3@gmail.com>
2026-02-19 22:17:53 +0300

    style(header): remove user avatar

--- src/components/dashboard/Header.tsx
+++ src/components/dashboard/Header.tsx
@@ -1,4 +1,4 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
 
 export function Header() {
     return (
@@ -7,10 +7,7 @@ export function Header() {
                 <h1 className="text-lg font-semibold">Dashboard</h1>
             </div>
             <div className="flex items-center gap-4">
-                <Avatar>
-                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-                    <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
+
             </div>
         </header>
     )
